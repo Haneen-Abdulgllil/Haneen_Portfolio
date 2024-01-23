e88df5199e83ed2360b51bd50e07699798efde82
Haneen-Abdulgllil <haneenabdulglil2018@yahoo.com>
2024-01-23 11:16:53 +0300

    add speed dial component

---
+++ src/pages/home.js
@@ -0,0 +1,14 @@
+
+// import NavBar from "../components/NavBar";
+import  MegaMenu from "../components/MegaMenu";
+import Footer from "../components/Footer_cus";
+import SpeedDial from "../components/SpeedDial";
+export default function Home() {
+  return (
+    <div className="container">
+        <MegaMenu/>
+        <SpeedDial/>
+        <Footer/>
+    </div>
+  )
+}

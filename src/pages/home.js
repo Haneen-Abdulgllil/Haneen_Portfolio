8a4c02c6abb144425ea841da7fe08c0f42446419
Haneen-Abdulgllil <haneenabdulglil2018@yahoo.com>
2024-01-24 13:25:34 +0300

    start desiging hero section

--- src/pages/home.js
+++ src/pages/home.js
@@ -7,6 +7,12 @@ export default function Home() {
   return (
     <div className="container">
         <MegaMenu/>
+        <div className="container m-5">
+          <div className="flex flex-col sm:flex-row items-center">
+            <div className="basis-1/2 bg-red-500">01</div>
+            <div className="basis-1/2 bg-blue-500">02</div>
+          </div>
+        </div>
         <SpeedDial/>
         <Footer/>
     </div>
